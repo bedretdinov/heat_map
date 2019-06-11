6ca14f1023d91b2cd550f39642fe9401346234d4
Nadir <bedretdinoff@mail.ru>
2019-06-11 15:56:33 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='heat_map',
+   version='1.0',
+   description='heat map with yandex map',
+   author='Nader Bedretdinov',
+   author_email='php-job@mail.ru',
+   packages=['heat_map'] 
+)
